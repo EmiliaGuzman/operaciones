8e02be04c2141ca7e9b8dd319cbb87f19b5ce476
EmiliaGuzman <54367844+EmiliaGuzman@users.noreply.github.com>
2020-04-07 08:38:21 -0500

    Update main.cpp

--- MenorDeTresNumeros/main.cpp
+++ MenorDeTresNumeros/main.cpp
@@ -12,21 +12,21 @@ int main()
     std::cin >> x;
     std::cout << " Ingrese un numero: ";
     std::cin >> y;
-    std::cout << "La suma es " << x+y << std::endl;
+    std::cout << "El resultado de la suma es " << x+y << std::endl;
     
     std::cout << " Resta de dos numeros\n";
     std::cout << " Ingrese un numero: ";
     std::cin >> x;
     std::cout << " Ingrese un numero: ";
     std::cin >> y;
-    std::cout << "La resta es " << x-y << std::endl;
+    std::cout << "El resultado de la resta es " << x-y << std::endl;
     
-    std::cout << " Division de dos numeros\n";
+    std::cout << " Multiplicacion de dos numeros\n";
     std::cout << " Ingrese un numero: ";
     std::cin >> x;
     std::cout << " Ingrese un numero: ";
     std::cin >> y;
-    std::cout << "La division es " << x/y << std::endl;
+    std::cout << "El resultado de la multiplicacion es " << x*y << std::endl;
     
     
     return 0;
